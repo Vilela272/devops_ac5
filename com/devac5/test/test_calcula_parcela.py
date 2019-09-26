2adc43c87eaf05f940c3c8f4d687cfc87ac527d8
Guilherme Vilela Moreira <1900486@SALAS.AULAS>
2019-09-26 20:28:50 -0300

    modificando arquivos

--- com/devac5/test/test_calcula_parcela.py
+++ com/devac5/test/test_calcula_parcela.py
@@ -1,7 +1,7 @@
 import pytest
 from com.devac5.calcula_parcela import valorPagamento
 
-	def valorPagamento():
-		calcula_parcela = valorPagamento
-		assert valorPagamento.([10, 2]) == 10.50 , "O resultado será 10.50"
+def valorPagamento():
+	calcula_parcela = valorPagamento
+	assert valorPagamento.([10, 2]) == 10.50 , "O resultado será 10.50"
 
--- com/devac5/test/test_converte_hora.py
+++ com/devac5/test/test_converte_hora.py
@@ -1,7 +1,7 @@
 import pytest
 from com.devac5.converte_hora import converteHora
 
-	def converteHora():
-		converte_hora = converteHora
-		assert converteHora.([15, 22]) == 03:22 PM , "O resultado será 03:22 PM"
+def converteHora():
+	converte_hora = converteHora
+	assert converteHora.([15, 22]) == 03:22 PM , "O resultado será 03:22 PM"
 

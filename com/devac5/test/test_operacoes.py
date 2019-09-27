01c27972ff890591bd47c891941364b7798337b0
Guilherme Vilela Moreira <1900486@SALAS.AULAS>
2019-09-26 21:10:09 -0300

    modificando arquivos_10

--- com/devac5/test/test_operacoes.py
+++ com/devac5/test/test_operacoes.py
@@ -2,6 +2,6 @@ import pytest
 from com.devac5.operacoes import Multiplicacao
 
 
-	def test_mult():
-		operacoes = Multiplicacao
-		assert test_mult([5,5]) == 25, "Resultado 25" 
+def test_mult():
+	operacoes = Multiplicacao
+	assert test_mult([5,5]) == 25, "Resultado 25" 
